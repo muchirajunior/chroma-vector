be7fa11d75f144c1faade105dec90657786328b4
MUCHIRAJUNIOR <muchirajunior022@gmail.com>
2026-09-19 12:08:56 +0300

    add delete

---
+++ vehicles/delete_record.py
@@ -0,0 +1,9 @@
+import chromadb 
+
+client = chromadb.PersistentClient(path='../chroma_db')
+
+collection =  client.get_collection('vehicles')
+
+collection.delete(ids='car')
+
+print(collection.get())
